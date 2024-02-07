b8a9259c100d83ad770094f94471e9ab7dfcdfbe
Chernet Tafesse <120357658+Jrkusha5@users.noreply.github.com>
2024-02-07 10:07:50 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose =require('mongoose')
+const router=require('./routes/book-route')
+
+const app= express()
+
+//middlewares
+
+app.use('/books', router)
+
+
+mongoose.connect
+("mongodb+srv://jrkusha5:YLsMt4HEZRfALbLn@cluster0.v66xvn6.mongodb.net/?retryWrites=true&w=majority"
+).then(()=> console.log("connected to DB"))
+.then(()=>{
+    app.listen(5000);
+}).catch((err)=>console.log(err))
+
+
+
+
+//YLsMt4HEZRfALbLn
